59a37c45aee054c204df3efba59527e0d9cb7c6f
Kyle Vu <ktvu@hbuhsd.edu>
2024-11-21 08:25:12 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,33 +2,32 @@ import java.util.Scanner;
 
 public class Main
 {
-  public static void main(String[] args)
-  {
-    // Make sure to test your methods here
-  }
+	public static void main(String[] args)
+	{
+		// No need to touch anything in here
+		Scanner sc = new Scanner(System.in);
+		String str = null;
 
-  public static void printNTimes(String word, int N)
-  {
-    // code solution here
-  }
+		System.out.println("Enter a sentence:");
+		str = sc.nextLine();
 
-  public static void printNums()
-  {
-    // code solution here
-  }
+		algorithm1(str);
+		algorithm2(str);
 
-  public static void uprightNumberTriangle(int N)
-  {
-    // code solution here
-  }
+		sc.close();
+	}
 
-  public static void starTree()
-  {
-    // code solution here
-  }
+	// This method should implement Algorithm 1 and print all neccessary data
+	public static void algorithm1(String input)
+	{
+		// TODO: Put solution to Algorithm 1 here
+		System.out.println("Implement me!");
+	}
 
-  public static void multTable()
-  {
-    // code solution here
-  }
+	// This method should implement Algorithm 2 and print all neccessary data
+	public static void algorithm2(String input)
+	{
+		// TODO: Put solution to Algorithm 1 here
+		System.out.println("Implement me!");
+	}
 }
